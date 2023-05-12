b0a6e800b4fffd3c007a93e0d4c22c555cd02ef1
Mriv31 <martin.rieu@gmail.com>
2023-05-12 11:31:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,7 @@ import sys
 from pathlib import Path
 import cmake_build_extension
 import setuptools
-from distutils.util import convert_path, dir_util
-
-from wheel.pep425tags import get_supported
-
-if dir_util.is_file('dist/PyTrackBead-*.whl'):
-    print('Found built wheel, installing...')
-    whl_path = dir_util.get_path('dist/PyTrackBead-*.whl')
-    whl_info = wheel.Wheel(whl_path)
-    if whl_info.supported(version.LooseVersion(sys.version), sys.platform):
-        print(whl_path)
-        pip install whl_path
-        return
-    else:
-        print('Wheel is not compatible with this Python version or architecture')
+from distutils.util import convert_pathrsion or architecture')
 
 init_py = inspect.cleandoc(
     """
